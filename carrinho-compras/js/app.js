38c09c21983a6631e5ec1d2c3281ed1e04da1491
Marcélio G. Pereira <marcelio.pereira@tse.jus.br>
2025-10-26 00:49:25 -0300

    Aula 4
    Conclusão da Aula 04 - Carrinho de Compras

---
+++ carrinho-compras/js/app.js
@@ -0,0 +1,49 @@
+let totalGeral;
+limpar();
+
+function adicionar() {
+    //recuperar valores nome do produto, quantidade e valor 
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
+    let quantidade = document.getElementById('quantidade').value;
+    // alert(nomeProduto);
+    // alert(valorUnitario);
+    // alert(quantidade.value);
+
+    // Verificar se o produto selecionado é válido
+    if (!produto || produto.trim() === "") {
+        alert("Selecione um produto válido.");
+        return;
+    }
+
+    // Verificar se a quantidade inserida é válida
+    if (isNaN(quantidade) || quantidade <= 0) {
+        alert("Insira uma quantidade válida.");
+        return;
+    }
+    
+    //calcular o preço, o nosso subtotal 
+    let preco = quantidade * valorUnitario;
+    // alert(preco);
+
+    //adicionar no carrinho 
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+        <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span Class="texto-azul">R$ ${preco}</span>
+    </section>`;
+
+    //atualizar o valor total
+    totalGeral = totalGeral + preco;
+    let campoTotal = document.getElementById('valor-total'); 
+    campoTotal.textContent =  `R$ ${totalGeral}`;
+    document.getElementById('quantidade').value = 0;
+
+}
+
+function limpar() {
+    totalGeral = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$ 0';
+    document.getElementById('quantidade').value = 0;
+}
